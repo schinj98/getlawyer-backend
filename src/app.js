e2b74036590df8a5ddc7ad221f66c0d8c1d43791
Sachin Jangid <sachinreal2003@gmail.com>
2026-01-01 21:32:54 +0530

    addeed s233

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import express from "express";
-import cors from "cors";
 
 import blogRoutes from "./routes/blog.routes.js";
 import leadRoutes from "./routes/lead.routes.js";
@@ -8,35 +7,14 @@ import cmsRoutes from "./routes/cms.routes.js";
 
 const app = express();
 
-/* -------------------- PUBLIC CORS (BLOGS) -------------------- */
-app.use(
-  "/blog",
-  cors({
-    origin: "*",              // 🔓 public
-    methods: ["GET"],
-  })
-);
-
-/* -------------------- PRIVATE CORS (CMS / AUTH) -------------------- */
-const privateCors = cors({
-  origin: [
-    "http://localhost:3000",
-    "https://getlawyer.me",
-    "https://www.getlawyer.me",
-  ],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-});
-
 /* -------------------- BODY PARSERS -------------------- */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 /* -------------------- ROUTES -------------------- */
-app.use("/blog", blogRoutes);        // 🔓 public
-app.use("/auth", privateCors, authRoutes);
-app.use("/cms", privateCors, cmsRoutes);
-app.use(privateCors, leadRoutes);    // form submit etc
+app.use("/blog", blogRoutes);
+app.use(leadRoutes);
+app.use(authRoutes);
+app.use(cmsRoutes);
 
 export default app;
