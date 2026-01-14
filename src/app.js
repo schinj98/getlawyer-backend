24c64b21990a30c53286f9e21c648795bfe29e3a
Sachin Jangid <sachinreal2003@gmail.com>
2026-01-14 15:07:18 +0530

    added cms5

--- src/app.js
+++ src/app.js
@@ -4,13 +4,25 @@ import blogRoutes from "./routes/blog.routes.js";
 import leadRoutes from "./routes/lead.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 import cmsRoutes from "./routes/cms.routes.js";
+import cors from "cors";
 
 const app = express();
+app.set("trust proxy", 1);
+
+app.use(
+  cors({
+    origin: "https://www.getlawyer.me",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.options("*", cors());
+
 
 /* -------------------- BODY PARSERS -------------------- */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set("trust proxy", 1);
 /* -------------------- ROUTES -------------------- */
 app.use("/blog", blogRoutes);
 app.use(leadRoutes);
