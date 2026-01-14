564d03130fc3094e6b279bb23ad24a9b55f4b881
Sachin Jangid <sachinreal2003@gmail.com>
2026-01-14 18:12:53 +0530

    added bliogs n backendss

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,5 +4,18 @@ import { login } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.post("/cms/login", login);
-
+router.post("/cms/logout", (req, res) => {
+    res.clearCookie("cms_auth", {
+      domain: ".getlawyer.me",
+      path: "/",
+    });
+  
+    res.clearCookie("cms_role", {
+      domain: ".getlawyer.me",
+      path: "/",
+    });
+  
+    res.json({ success: true });
+  });
+  
 export default router;
