836ffe260a756e61ba00f5b0c6a74f014ab27e73
Sachin Jangid <sachinreal2003@gmail.com>
2026-01-15 20:21:44 +0530

    updatdfsssss

--- server.js
+++ server.js
@@ -1,21 +1,10 @@
-// import dotenv from "dotenv";
-// dotenv.config();
+import dotenv from "dotenv";
+dotenv.config();
 
-// import app from "./src/app.js";
+import app from "./src/app.js";
 
-// const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 5050;
 
-// app.listen(PORT, "0.0.0.0", () => {
-//   console.log(`🚀 Backend running on port ${PORT}`);
-// });
-import express from "express";
-
-const app = express();
-
-app.get("/health", (req, res) => {
-  res.send("OK");
-});
-
-app.listen(5050, "127.0.0.1", () => {
-  console.log("🚀 IPv4 ONLY server running on 127.0.0.1:5050");
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Backend running on port ${PORT}`);
 });
