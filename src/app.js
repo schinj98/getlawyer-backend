0425504784a6b587cf21f789f6e1f72f41ca3935
Sachin Jangid <sachinreal2003@gmail.com>
2026-01-16 21:45:14 +0530

    updatdfsssssxsss

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import express from "express";
-import cors from "cors";
 import blogRoutes from "./routes/blog.routes.js";
 import leadRoutes from "./routes/lead.routes.js";
 import authRoutes from "./routes/auth.routes.js";
@@ -8,42 +7,6 @@ import cmsRoutes from "./routes/cms.routes.js";
 const app = express();
 
 app.set("trust proxy", 1);
-
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://www.getlawyer.me",
-  "https://getlawyer.me",
-];
-
-/* -------------------- MANUAL CORS HANDLING -------------------- */
-// This prevents duplicate headers by manually setting them
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-
-  // Check if origin is allowed
-  if (!origin || allowedOrigins.includes(origin)) {
-    // Only set if not already set (prevents duplicates)
-    if (!res.getHeader('Access-Control-Allow-Origin')) {
-      res.setHeader('Access-Control-Allow-Origin', origin || allowedOrigins[0]);
-    }
-    if (!res.getHeader('Access-Control-Allow-Credentials')) {
-      res.setHeader('Access-Control-Allow-Credentials', 'true');
-    }
-    if (!res.getHeader('Access-Control-Allow-Methods')) {
-      res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH,OPTIONS');
-    }
-    if (!res.getHeader('Access-Control-Allow-Headers')) {
-      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    }
-  }
-
-  // Handle preflight
-  if (req.method === 'OPTIONS') {
-    return res.status(204).end();
-  }
-
-  next();
-});
 
 /* -------------------- BODY PARSERS -------------------- */
 app.use(express.json());
